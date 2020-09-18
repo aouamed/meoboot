40854bc111967f6493e5db40eab061fd93ffde7c
aouamed <rabbat2@hotmail.fr>
2020-09-18 11:21:41 +0100

    Update ex_init.py

--- ex_init.py
+++ ex_init.py
@@ -2,8 +2,7 @@
 
 import sys, meoboot
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 5:
 	pass
 else:
-	meoboot.MeobootMainEx(sys.argv[1], sys.argv[2], sys.argv[3])
-
+	meoboot.MeobootMainEx(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
