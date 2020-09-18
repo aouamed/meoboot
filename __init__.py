b240eab2415da7b0853b41b9adeda4d2826ce5e4
aouamed <rabbat2@hotmail.fr>
2020-09-18 11:20:30 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,22 +1,30 @@
+# uncompyle6 version 3.7.4
+# Python bytecode 2.7 (62211)
+# Decompiled from: Python 3.8.2 (default, Jul 16 2020, 14:00:26) 
+# [GCC 9.3.0]
+# Embedded file name: /usr/lib/enigma2/python/Plugins/Extensions/MeoBoot/__init__.py
+# Compiled at: 2014-02-10 23:23:49
 from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
-import os,gettext
-PluginLanguageDomain = "MeoBoot"
-PluginLanguagePath = "Extensions/MeoBoot/po"
+import os, gettext
+PluginLanguageDomain = 'MeoBoot'
+PluginLanguagePath = 'Extensions/MeoBoot/po'
 
 def localeInit():
-	lang = language.getLanguage()[:2] # getLanguage returns e.g. "fi_FI" for "language_country"
-	os.environ["LANGUAGE"] = lang # Enigma doesn't set this (or LC_ALL, LC_MESSAGES, LANG). gettext needs it!
-	print "[MeoBoot] set language to ", lang
-	gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
+    lang = language.getLanguage()[:2]
+    os.environ['LANGUAGE'] = lang
+    print '[MeoBoot] set language to ', lang
+    gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
+
 
 def _(txt):
-	t = gettext.dgettext(PluginLanguageDomain, txt)
-	if t == txt:
-		print "[MeoBoot] fallback to default translation for", txt
-		t = gettext.dgettext('enigma2', txt)
-	return t
+    t = gettext.dgettext(PluginLanguageDomain, txt)
+    if t == txt:
+        print '[MeoBoot] fallback to default translation for', txt
+        t = gettext.dgettext('enigma2', txt)
+    return t
 
 
 localeInit()
 language.addCallback(localeInit)
+# okay decompiling __init__.pyo
