fcc356c260c2a3c16afbc4b8f53cc33fe187e835
aouamed <rabbat2@hotmail.fr>
2020-09-18 13:00:31 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+import setup_translate
+
+
+setup(name = 'enigma2-plugin-extensions-meoboot',
+		version='1.4.1',
+		author='aouamedj',
+		author_email='aouamed@gmail.com',
+		package_dir = {'Extensions.Meoboot': 'src'},
+		packages=['Extensions.Meoboot'],
+		package_data={'Extensions.Meoboot': ['*.png', '*.sh']},
+		description = 'Meoboot and manual flashing image',
+		cmdclass = setup_translate.cmdclass,
+	)
+
+
